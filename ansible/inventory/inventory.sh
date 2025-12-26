77af14bfa118a4f1f3f771d3751cfd319a77a8bc
Dmytro Korobko <dmytro.korobko@gmail.com>
2025-12-26 00:14:47 -0500

    Add dynamic Ansible inventory based on Terraform outputs

--- ansible/inventory/inventory.sh
+++ ansible/inventory/inventory.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+set -e
 
+if [[ "$1" == "--list" ]]; then
+   IPLIST=$(terraform -chdir="../terraform/02_compute" output -json ec2_public_ip)
+   jq -n --argjson ips "$IPLIST" '{
+      servers: {
+         hosts: $ips
+      }
+   }'
+else
+   echo "{}"
+fi
