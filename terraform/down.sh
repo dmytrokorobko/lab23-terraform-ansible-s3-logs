83bf75213d6014302e44430b87aede3f951d3e7c
Dmytro Korobko <dmytro.korobko@gmail.com>
2025-12-25 23:56:14 -0500

    Add helper scripts to apply and destroy Terraform layers in order

--- terraform/down.sh
+++ terraform/down.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+set -e
+terraform -chdir=./02_compute destroy
+terraform -chdir=./01_network destroy
--- terraform/up.sh
+++ terraform/up.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+set -e
+terraform -chdir=./01_network apply
+terraform -chdir=./02_compute apply
