ceabe5039cc0d5937b7065c1a22872ec326d13a8
Dmytro Korobko <dmytro.korobko@gmail.com>
2025-12-25 21:55:55 -0500

    Exclude unsupported AZs and make subnet placement deterministic

--- terraform/01_network/subnets.tf
+++ terraform/01_network/subnets.tf
@@ -1,18 +1,20 @@
 data "aws_availability_zones" "azs" {}
 
+locals {
+  vpc_cidr_prefix = tonumber(split("/", var.vpc_cidr)[1])
+  true_azs = setsubtract(data.aws_availability_zones.azs.names, ["us-east-1e"])
+}
+
 check "enough_azs" {
   assert {
-    condition = var.subnets_count <= length(data.aws_availability_zones.azs.names)
+    condition = var.subnets_count <= length(local.true_azs)
     error_message = "Current region doesn't have enough availability zones to create ${var.subnets_count} subnets"
   }
 }
 
-locals {
-  vpc_cidr_prefix = tonumber(split("/", var.vpc_cidr)[1])
-}
-
 resource "aws_subnet" "subnets" {
   count = var.subnets_count
+  availability_zone = local.true_azs[count.index]
   cidr_block = cidrsubnet(
    var.vpc_cidr,
    var.subnet_cidr_prefix - local.vpc_cidr_prefix,
